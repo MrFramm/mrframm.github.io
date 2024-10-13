2dfc8d2870f3f1c1f72ba33a39b928e19728482a
MrFramm <119941587+MrFramm@users.noreply.github.com>
2024-10-13 18:40:30 +0500

    Update scr2.js

--- scr2.js
+++ scr2.js
@@ -1,11 +1,9 @@
 
   const app = new PIXI.Application();
-  await app.init({ width: 640, height: 360 });
+  await app.init({background: '#1099bb',  resizeTo: window });
   document.body.appendChild(app.canvas);
-      // load the PNG asynchronously
-  await PIXI.Assets.load('https://pixijs.com/assets/files/sample-747abf529b135a1f549dff3ec846afbc.png');
-  let sprite = PIXI.Sprite.from('https://pixijs.com/assets/files/sample-747abf529b135a1f549dff3ec846afbc.png');
-  app.stage.addChild(sprite);
+      
+  await preload();
       // Add a variable to count up the seconds our demo has been running
   let elapsed = 0.0;
   // Tell our application's ticker to run a new callback every frame, passing
@@ -17,3 +15,17 @@
     // by 50 to slow the animation down a bit...
     sprite.x = 100.0 + Math.cos(elapsed/50.0) * 100.0;
   });
+async function preload()
+{
+    const assets = [
+        { alias: 'background', src: 'https://pixijs.com/assets/tutorials/fish-pond/pond_background.jpg' },
+        { alias: 'fish1', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish1.png' },
+        { alias: 'fish2', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish2.png' },
+        { alias: 'fish3', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish3.png' },
+        { alias: 'fish4', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish4.png' },
+        { alias: 'fish5', src: 'https://pixijs.com/assets/tutorials/fish-pond/fish5.png' },
+        { alias: 'overlay', src: 'https://pixijs.com/assets/tutorials/fish-pond/wave_overlay.png' },
+        { alias: 'displacement', src: 'https://pixijs.com/assets/tutorials/fish-pond/displacement_map.png' },
+    ];
+    await Assets.load(assets);
+}
