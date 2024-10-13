eb852f39c7fa8ad9daa0eb6a2ea033e977f0da61
MrFramm <119941587+MrFramm@users.noreply.github.com>
2024-10-13 16:24:42 +0500

    Update scr2.js

--- scr2.js
+++ scr2.js
@@ -1,14 +1,4 @@
-import { Application } from ./pixi.js';
 
-// Asynchronous IIFE
-(async () =>
-{
-    // Create a PixiJS application.
-    const app = new Application();
 
-    // Intialize the application.
-    await app.init({ background: '#1099bb', resizeTo: window });
-
-    // Then adding the application's canvas to the DOM body.
-    document.body.appendChild(app.canvas);
-})();
+ const app = new PIXI.Application();
+  await app.init({ width: 640, height: 360 });
