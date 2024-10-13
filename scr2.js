fea3aac34a89d6f08734b388ca2ec6c077590762
MrFramm <119941587+MrFramm@users.noreply.github.com>
2024-10-13 19:27:10 +0500

    Update scr2.js

--- scr2.js
+++ scr2.js
@@ -47,11 +47,44 @@ else
   background.x = app.screen.width / 2;
 background.y = app.screen.height / 2;
 }
+function addFishes(app, fishes)
+{
+   const fishContainer = new Container();
+
+app.stage.addChild(fishContainer);
+const fishCount = 20;
+const fishAssets = ['fish1', 'fish2', 'fish3', 'fish4', 'fish5'];
+  for (let i = 0; i < fishCount; i++)
+{
+    const fishAsset = fishAssets[i % fishAssets.length];
+    const fish = Sprite.from(fishAsset);
+
+    fish.anchor.set(0.5);
+
+    fish.direction = Math.random() * Math.PI * 2;
+    fish.speed = 2 + Math.random() * 2;
+    fish.turnSpeed = Math.random() - 0.8;
+
+    fish.x = Math.random() * app.screen.width;
+    fish.y = Math.random() * app.screen.height;
+    fish.scale.set(0.5 + Math.random() * 0.2);
+
+    fishContainer.addChild(fish);
+    fishes.push(fish);
+}
+  
+}
+export function animateFishes(app, fishes, time)
+{
+    /** -- INSERT CODE HERE -- */
+}
 (async () =>
 {
  
     await preload();
 
     addBackground(app);
+  
+    addFishes(app, fishes);
 })();
 
